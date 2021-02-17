dbec0844b1e0f3570212c64aeee63c8a423e7dd6
Busola Olaposi <78625188+busolaaofficial@users.noreply.github.com>
2021-02-17 15:56:50 +0100

    Add files via upload

---
+++ top_design.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title> ~BTWEB~ </title>
+<link rel="Shortcut Icon"  href="images/applelap.jpg"/>
+<link  href="styles/style.css"  rel="stylesheet" type="text/css"/>
+<script src="javascripts/jquery.js" type="text/javascript"></script>
+</head>
+<body>
+
+<nav>
+
+<!-- <div  class="logoDiv">
+
+<span style="margin-left:70%; width:200%;color:#c18f59"><b>~BTWEB~</b></span>
+</div>
+<span style="margin-left:85%;  font-size:30px; color:#c18f59">
+    &#9776;
+  </span> -->
+
+
+</nav>
+
+</body>
+
+
+
+</html>
